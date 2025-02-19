71f2492eac9e46970acd6999f3673a897a0347f2
digitpic <jkw5033@gmail.com>
2025-02-19 22:33:44 +0900

    chore: set up project

---
+++ src/test/kotlin/com/mandamong/api/KotlinSpringMandamongApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.mandamong.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringMandamongApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
