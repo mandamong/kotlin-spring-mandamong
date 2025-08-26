ed794b121ec2e529e37fd8c3dfc1b60c4196f6c4
Kyuwon Jeong <jkw5033@gmail.com>
2025-08-26 20:39:36 +0900

    refactor: 쿠키 사용 로직 제거

--- src/main/kotlin/com/mandamong/server/auth/controller/AuthController.kt
+++ src/main/kotlin/com/mandamong/server/auth/controller/AuthController.kt
@@ -6,8 +6,6 @@ import com.mandamong.server.auth.service.AuthService
 import com.mandamong.server.common.constants.ApiPath
 import com.mandamong.server.common.dto.ApiResponse
 import com.mandamong.server.user.dto.LoginUser
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PostMapping
@@ -22,18 +20,15 @@ class AuthController(
     @PostMapping(ApiPath.Auth.LOGIN)
     fun login(
         @RequestBody request: LoginRequest,
-        response: HttpServletResponse,
     ): ResponseEntity<ApiResponse<LoginResponse>> {
-        return ApiResponse.ok(service.login(request.email, request.password, response))
+        return ApiResponse.ok(service.login(request.email, request.password))
     }
 
     @PostMapping(ApiPath.Auth.LOGOUT)
     fun logout(
         @AuthenticationPrincipal loginUser: LoginUser,
-        request: HttpServletRequest,
-        response: HttpServletResponse,
     ): ResponseEntity<ApiResponse<Nothing>> {
-        service.logout(loginUser.userId, request, response)
+        service.logout(loginUser.userId)
         return ApiResponse.deleted()
     }
 
--- src/main/kotlin/com/mandamong/server/auth/service/AuthService.kt
+++ src/main/kotlin/com/mandamong/server/auth/service/AuthService.kt
@@ -3,13 +3,10 @@ package com.mandamong.server.auth.service
 import com.mandamong.server.auth.dto.LoginResponse
 import com.mandamong.server.auth.repository.RefreshTokenRepository
 import com.mandamong.server.common.error.exception.BadRequestException
-import com.mandamong.server.common.util.cookie.CookieUtil
 import com.mandamong.server.common.util.jwt.TokenUtil
 import com.mandamong.server.common.util.log.log
 import com.mandamong.server.infrastructure.minio.service.MinioService
 import com.mandamong.server.user.service.UserService
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -21,27 +18,23 @@ class AuthService(
     private val passwordEncoder: BCryptPasswordEncoder,
     private val refreshTokenRepository: RefreshTokenRepository,
     private val minioService: MinioService,
-    private val cookieUtil: CookieUtil,
 ) {
 
     @Transactional
-    fun login(email: String, password: String, response: HttpServletResponse): LoginResponse {
+    fun login(email: String, password: String): LoginResponse {
         val savedUser = userService.getByEmail(email)
         validatePassword(password, savedUser.password)
         val accessToken = tokenUtil.createAccessToken(savedUser.id)
         val refreshToken = tokenUtil.createRefreshToken(savedUser.id)
         refreshTokenRepository.set(savedUser.id, refreshToken)
         val presignedUrl = minioService.getPresignedUrlByObjectKey(savedUser.imageKey)
-        cookieUtil.add(response, "access_token", accessToken, 300)
-        cookieUtil.add(response, "refresh_token", refreshToken, 2_592_000)
         log().info("USER_LOGIN userId=${savedUser.id}")
         return savedUser.toDto(presignedUrl, accessToken, refreshToken)
     }
 
     @Transactional
-    fun logout(userId: Long, request: HttpServletRequest, response: HttpServletResponse) {
+    fun logout(userId: Long) {
         refreshTokenRepository.delete(userId)
-        cookieUtil.delete(request, response, "access_token")
         log().info("USER_LOGOUT userId=$userId")
     }
 
