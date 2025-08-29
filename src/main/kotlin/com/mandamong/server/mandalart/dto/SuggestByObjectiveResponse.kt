61b311ffeb537afbe69bf4e5ac8a20ded88b8eaf
Kyuwon Jeong <jkw5033@gmail.com>
2025-08-29 16:45:37 +0900

    refactor: 타입 추론을 이용해 dto 변환 로직 없이 역직렬화

--- src/main/kotlin/com/mandamong/server/mandalart/dto/SuggestByObjectiveResponse.kt
+++ src/main/kotlin/com/mandamong/server/mandalart/dto/SuggestByObjectiveResponse.kt
@@ -1,19 +1,5 @@
 package com.mandamong.server.mandalart.dto
 
-import com.fasterxml.jackson.databind.JsonNode
-
 data class SuggestByObjectiveResponse(
     val actions: List<String>,
-) {
-
-    companion object {
-        fun from(json: JsonNode): SuggestByObjectiveResponse {
-            return SuggestByObjectiveResponse(
-                actions = json.get(ACTIONS_KEY).map { it.asText() }
-            )
-        }
-
-        private const val ACTIONS_KEY = "actions"
-    }
-
-}
+)
