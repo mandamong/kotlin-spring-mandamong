e3cc49f7861764fed9c708b4d4054f7821d278bc
Kyuwon Jeong <jkw5033@gmail.com>
2025-08-29 15:04:05 +0900

    refactor: JsonAlias 설정으로 클라이언트 요청 받을 수 있도록 수정

--- src/main/kotlin/com/mandamong/server/mandalart/dto/SuggestBySubjectRequest.kt
+++ src/main/kotlin/com/mandamong/server/mandalart/dto/SuggestBySubjectRequest.kt
@@ -1,5 +1,8 @@
 package com.mandamong.server.mandalart.dto
 
+import com.fasterxml.jackson.annotation.JsonAlias
+
 data class SuggestBySubjectRequest(
+    @JsonAlias(value = ["subject"])
     val question: String,
 )
